94e8e719cf4d2c7f8b9fa25916d78d79fa637a69
dilner1 <dan.milner871@gmail.com>
2021-08-31 12:23:47 +0000

    Collision working

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
         obstacle.style.left = obstaclePosition + 'px'
 
         let timerId = setInterval(function() {
-            if (obstaclePosition === 0) {
+            if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
 
                     clearInterval(timerId)
                     alertMessage.innerHTML = 'Game Over'
